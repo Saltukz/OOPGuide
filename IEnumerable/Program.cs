dde70eab92862e8161a922fcd03746e4127d898b
saltukzz <s.uzungungor@gmail.com>
2022-07-12 11:52:26 +0300

    IEnumerable

---
+++ IEnumerable/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace IEnumerableExample
+{
+    public class Program
+    {
+        static void Main()
+        {
+            //foreach(string str in GetDemoEnumerable())
+            //{
+            //    Console.WriteLine(str);
+            //}
+
+            int[] data = { 1, 2, 3,4,5 };
+            SingleDim arry = new SingleDim(data);
+            Console.WriteLine("array elements are ");
+
+            foreach(int item in arry)
+            {
+                Console.WriteLine(item);
+            }
+
+
+        }
+
+        static IEnumerable<string> GetDemoEnumerable()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                yield return i.ToString();
+
+            }
+            yield return "numbers over";
+        }
+    }
+
+   
+
+
+}
---
+++ IEnumerable/SingleDim.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IEnumerableExample
+{
+    public class SingleDim : IEnumerable
+    {
+        public int[] array;
+        public SingleDim(int[] array)
+        {
+            this.array = array;
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            return new ArrayEnumerator(this);
+        }
+
+        public class ArrayEnumerator : IEnumerator
+        {
+            private int loc = -1;
+            private SingleDim a;
+
+            public ArrayEnumerator(SingleDim a)
+            {
+                this.a = a;
+            }
+
+            public object Current => a.array[loc];
+
+            public bool MoveNext()
+            {
+                if(loc< a.array.Length - 1)
+                {
+                    loc++;
+                    return true;
+                }
+                return false;
+            }
+
+            public void Reset()
+            {
+                loc= -1;
+            }
+        }
+    }
+}
