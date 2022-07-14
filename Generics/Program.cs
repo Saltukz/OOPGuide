96e2c606761cd15a98686d1bf9544747d88136b0
saltukzz <s.uzungungor@gmail.com>
2022-07-14 15:21:16 +0300

    generic

---
+++ Generics/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Generics
+{
+    class Program
+    {
+        static void Main()
+        {
+            int a, b;
+            string p,q;
+            a = 5;
+            b = 10;
+            Console.WriteLine("Before swap, a={0} and b={1}", a, b);
+            Swap(ref a, ref b);
+            Console.WriteLine("After swap , a={0} and b={1}",a,b);
+            p = "Hello";
+            q = "World";
+            Console.WriteLine("Before swap p={0} and q={1}",p,q);
+            Swap(ref p, ref q);
+            Console.WriteLine("After swap p={0} and q={1}",p,q);
+        }
+
+        static void Swap<T> (ref T x, ref T y)
+        {
+            T temp;
+            temp = x;
+            x= y;
+            y= temp;
+        }
+    }
+}
---
+++ GenericsExample/Program.cs
@@ -0,0 +1,70 @@
+﻿namespace GenericsExample
+{
+    class Program
+    {
+        static void Main()
+        {
+            DummyList<int> myList = new DummyList<int>();
+            myList.Add(10);
+            myList.Add(20);
+            Console.WriteLine(myList.Remove(0));
+            Console.WriteLine(myList.RemoveFromEnd());
+            myList.Add(30);
+            myList.Add(40);
+            Console.WriteLine(myList.Remove(1));
+            Console.WriteLine(myList.RemoveFromEnd());
+
+        }
+
+        public class DummyList<T>
+        {
+
+            int size;
+            int indx = -1;
+            T[] items;
+
+            public DummyList() : this(10)
+            {
+
+            }
+
+            public DummyList(int x)
+            {
+                size = x;
+                items = new T[size];
+            }
+
+            public void Add(T item)
+            {
+                indx++;
+                if(indx >= size) throw new IndexOutOfRangeException();
+                items[indx] = item;
+            }
+
+            public T RemoveFromEnd()
+            {
+                if (indx >= 0)
+                {
+                    return items[indx--];
+                }
+                else throw new InvalidOperationException("The list is Empty");
+            }
+
+            public T Remove(int x)
+            {
+                if(indx >= 0)
+                {
+                    T item;
+                    item = items[x];
+                    for (int i = x; i < indx; i++) items[i] = items[i + 1];
+                        indx--;
+                    return item;
+
+                }
+
+                throw new InvalidOperationException("The list is empty");
+
+            }
+        }
+    }
+}
