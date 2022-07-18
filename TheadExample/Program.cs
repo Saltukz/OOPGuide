ee7b0999311da2a9eb04abcd481e1e915104bc96
saltukzz <s.uzungungor@gmail.com>
2022-07-18 15:23:19 +0300

    Thread

---
+++ TheadExample/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace ThreadExample
+{
+    class Program
+    {
+        static void Main()
+        {
+            ThreadStart deligobj = new ThreadStart(ThreadCode);
+            Thread childThread = new Thread(deligobj);
+            childThread.Start();
+            for(int i = 0; i <= 5; i++)
+            {
+                Console.WriteLine("This is the main thread: {0}",i);
+                Thread.Sleep(1000);
+            }
+        }
+
+        static void ThreadCode()
+        {
+            for(int i = 0; i <= 10; i++)
+            {
+                Console.WriteLine("This is child thread: {0}", i);
+                Thread.Sleep(500);
+            }
+        }
+    }
+}
