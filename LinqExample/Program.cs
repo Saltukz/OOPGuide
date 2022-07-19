1c94efa46561bd80e11f7492f323f2c320571585
saltukzz <s.uzungungor@gmail.com>
2022-07-19 16:26:06 +0300

    linqExample

---
+++ LinqExample/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace LinqExample
+{
+    class Program
+    {
+        static void Main()
+        {
+            string[] fruits = { "Apple", "Grapes", "Mango", "Banana", "Pineapple" };
+            IEnumerable<string> selectedFruits = from f in fruits where f.Length >= 6 orderby f select f;
+
+            foreach(var selectedFru in selectedFruits)
+            {
+                Console.WriteLine(selectedFru);
+            }
+        }
+    }
+}
